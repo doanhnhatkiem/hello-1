c3707a1fb62b9606f8f1555173b710158f51ff0b
doanhnhatkiem <doanhnhatkiem@users.noreply.github.com>
2019-04-28 09:49:11 +0700

    Updates

---
+++ precache-manifest.5412440ef2c86620e3eebb7b1fe81718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec575a4e86e732928e67",
+    "url": "/doanhnhatkiem/hello-1.git/static/js/runtime~main.7901bbed.js"
+  },
+  {
+    "revision": "62bbc5547e139d9c8e16",
+    "url": "/doanhnhatkiem/hello-1.git/static/js/main.d6e24e08.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/doanhnhatkiem/hello-1.git/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "62bbc5547e139d9c8e16",
+    "url": "/doanhnhatkiem/hello-1.git/static/css/main.d7beb415.chunk.css"
+  },
+  {
+    "revision": "e9d187660e6be7bdaafcae1b95b703e2",
+    "url": "/doanhnhatkiem/hello-1.git/index.html"
+  }
+];
